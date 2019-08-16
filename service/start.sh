08369ef68cd0c96a7a1cb89c922042b1411bf43d
Albert Puig <albert.puig@cern.ch>
2019-08-16 13:40:58 +0200

    Update start file.

--- service/start.sh
+++ service/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 now=$(date +"%Y %b %d %H:%M:%S")
 echo "Starting expense bot on $now"
-python3 /home/osmc/src/expense-bot/expensebot/cli.py
+python3 /home/osmc/src/expense-bot/expensebot/cli.py -c /home/osmc/.expensebotrc
 exit
